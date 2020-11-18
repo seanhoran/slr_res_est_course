caa35227583218da188f4cb877e76d16de7630dc
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-18 06:33:48 -0500

    Update pdac2021.py

--- pdac2021.py
+++ pdac2021.py
@@ -20,11 +20,6 @@ if exercise == radio_options[0]:
   st.image("055CF2A4-98DC-488C-B5A6-15CC02C9974E.png", width=100)
   st.title("Fundamentals of Resource Estimation")
   about_course.about_course()
-  st.write("")
-  st.markdown("## Presenters")
-  st.markdown("**Sean Horan**  <shoran@slrconsulting.com>")
-  st.image("49640CBC-E71B-4565-9307-0558A8B916B4.jpeg", width=200)
-  st.write("Can put bios here")
 if exercise == radio_options[1]:
   db_ex.db_ex()
 if exercise == radio_options[2]:
