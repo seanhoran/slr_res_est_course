cd24ec98f15a94ca69289b85566321f1307fb1e0
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-18 06:27:59 -0500

    Update about_course.py

--- exercises/about_course.py
+++ exercises/about_course.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import itertools
 import os
 
-path = flatten(os.getcwd().split("//")[:-1]) + "//text_blocks"
+path = itertools.chain(os.getcwd().split("//")[:-1]) + "//text_blocks"
 
 def about_course():
   f = open(path + "//about_course.txt", "r")
