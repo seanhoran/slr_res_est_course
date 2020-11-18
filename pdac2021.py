4d67c4a024bd845f7522dd407e7f679dabfd9576
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-17 22:44:28 -0500

    Update pdac2021.py

--- pdac2021.py
+++ pdac2021.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import db_ex
 
 radio_options = ["01 About the Course", 
                  "02 Database", 
@@ -26,5 +25,5 @@ if exercise == radio_options[0]:
   st.image("49640CBC-E71B-4565-9307-0558A8B916B4.jpeg", width=200)
   st.write("Can put bios here")
 if exercise == radio_options[1]:
-  st.write("db")
+  import db_ex
 
