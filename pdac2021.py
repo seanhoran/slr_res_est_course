0efa9040aebe42558294a87b405a21a979432654
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-17 21:48:56 -0500

    Update pdac2021.py

--- pdac2021.py
+++ pdac2021.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
-radio_options = ["01 About the Course", "02 Database", "03 Capping", "04 Compositing", "05 Variograms", "06 Interpolation", "07 Classification"]
-st.sidebar.radio("Pick Exercise", options=radio_options, index=0, key=None)
+radio_options = ["01 About the Course", 
+                 "02 Database", 
+                 "03 Capping", 
+                 "04 Compositing", 
+                 "05 Variograms", 
+                 "06 Interpolation", 
+                 "07 Classification"]
+
+exercise=st.sidebar.radio("Pick Exercise", 
+                          options=radio_options, 
+                          index=0, 
+                          key=None)
+
+if exercise == radio_options[0]:
+  st.title("Fundamentals of Resource Estimation")
 
