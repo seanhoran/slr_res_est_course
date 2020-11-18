8f37a24f73a751cc8b304f6e4e7d8304ae916db2
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-18 06:24:20 -0500

    Update about_course.py

--- exercises/about_course.py
+++ exercises/about_course.py
@@ -1,5 +1,8 @@
-import streamlit as t
+import streamlit as st
+import os
+
+path = os.getcwd().split("//")[:-1] + "//text_blocks"
 
 def about_course():
-  f = open("..//text_blocks//about_course.txt", "r")
+  f = open(path + "//about_course.txt", "r")
   st.write(f.read())
