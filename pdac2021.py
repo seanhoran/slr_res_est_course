5965c3e7b03b81f8b43b96fa02edebb7783c9b0f
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-17 21:42:07 -0500

    Update pdac2021.py

--- pdac2021.py
+++ pdac2021.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-radio_options = ["01 About the Course"]
+radio_options = ["01 About the Course", "02 Database", "03 Capping", "04 Compositing", "05 Variograms", "06 Interpolation", "07 Classification"]
 st.sidebar.radio("Pick Exercise", options=radio_options, index=0, key=None)
+
