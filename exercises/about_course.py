ba0a3efa5359056d1fae10c9229caae12fc4216a
seanhoran <38132944+seanhoran@users.noreply.github.com>
2020-11-18 06:35:58 -0500

    Update about_course.py

--- exercises/about_course.py
+++ exercises/about_course.py
@@ -2,8 +2,19 @@ import streamlit as st
 import itertools
 import os
 
-path = "..//pdac2021_res_est_course//text_blocks"
+
 
 def about_course():
+  
+  st.title("Fundametals of Resource Estimation")
+  path = "..//pdac2021_res_est_course//text_blocks"
   f = open(path + "//about_course.txt", "r")
   st.write(f.read())
+  
+    st.write("")
+  st.markdown("## Presenters")
+  
+  st.image("..//pdac2021_res_est_course//49640CBC-E71B-4565-9307-0558A8B916B4.jpeg", width=200)
+  st.markdown("**Sean Horan P.Geo - Technical Manager Geology**")
+  st.markdown("<shoran@slrconsulting.com>")
+  st.write("Can put bios here")
