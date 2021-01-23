83c56a94ba1705891bdebc5e65100f851b370482
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-22 21:56:28 -0500

    Update capping_ex.py

--- exercises/capping_ex.py
+++ exercises/capping_ex.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 def capping_ex():
   st.title("Capping Exercise")
-  options = ['None', 'Answer 1', 'right answer']
-  answer=st.radio('what is the answer', options=options, index=0, key='quest')
+  #options = ['None', 'Answer 1', 'right answer']
+  #answer=st.radio('what is the answer', options=options, index=0, key='quest')
   
-  if answer='right answer':
-    st.write('correct')
-  else:
-    st.write('wrong')
+  #if answer='right answer':
+   # st.write('correct')
+  #else:
+   # st.write('wrong')
     
     
