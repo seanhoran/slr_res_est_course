46fe6bb8d3fef138c60c680d534f7be52fa85f18
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-22 22:06:03 -0500

    Update capping_ex.py

--- exercises/capping_ex.py
+++ exercises/capping_ex.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
 def capping_ex():
+  st.markdown(
+    """ <style>
+            div[role="radiogroup"] >  :first-child{
+                display: none !important;
+            }
+        </style>
+        """,
+    unsafe_allow_html=True
+)
+  
   st.title("Capping Exercise")
   options = ['None', 'Answer 1', 'right answer']
   answer=st.radio('what is the answer', options=options, index=0, key='quest')
