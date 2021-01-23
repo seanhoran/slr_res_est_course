36946afc0ae0440427c8683ba9eaf91ca59c2e33
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-22 21:53:27 -0500

    Update capping_ex.py

--- exercises/capping_ex.py
+++ exercises/capping_ex.py
@@ -4,7 +4,7 @@ def capping_ex():
   st.title("Capping Exercise")
   options = ['None', 'Answer 1', 'right answer']
   answer=st.radio('what is the answer', options=options, index=0, key='quest')
-  st.write(answer)
+  
   if answer='right answer':
     st.write('correct')
   else:
