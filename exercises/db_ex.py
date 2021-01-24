f3ffbcf6c90c902aa421b10f0a090ea03bd4f355
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-23 20:25:12 -0500

    Update db_ex.py

--- exercises/db_ex.py
+++ exercises/db_ex.py
@@ -2,3 +2,20 @@ import streamlit as st
 
 def db_ex():
   st.write("db exercise")
+  from mpl_toolkits.mplot3d import Axes3D
+  import numpy as np
+  import matplotlib.pyplot as plt
+  N1 = 10
+  N2 = 10
+  N3 = 10
+  ma = np.random.choice([0,1], size=(N1,N2,N3), p=[0.99, 0.01])
+
+  fig = plt.figure()
+  ax = fig.gca(projection='3d')
+  ax.set_aspect('equal')
+
+  ax.voxels(ma, edgecolor="k")
+
+  #plt.show()
+  
+  st.pyplot(fig)
