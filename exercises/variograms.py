92257d0b327101f463071c7b0f742f3eb0908f59
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 13:45:20 -0500

    Update variograms.py

--- exercises/variograms.py
+++ exercises/variograms.py
@@ -1,5 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def variogram(nugget, range):
-  pass
+def variogram(nugget=0.0, srange=100.):
+  var = 1.-nugget
+  h = np.arange(150.)
+  gamma = nugget + var*((3*h)/(2*srange)-(h**3)/(2*srange**3))
+  gamma[h==0]=0.
+  gamma[h>srange]=1.0
+  return gamma, h; 
+
+def variograms():
+  gamma, h = variogram(0.3, 60.)
+  fig, ax = plt.subplots()
+  plt.plot(h, gamma, '-r')
+  st.pyplot(fig)
