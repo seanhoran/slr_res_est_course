f7c99271ec181b9b258277b2a765ba89fa527d06
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 09:24:11 -0500

    Update about_course.py

--- exercises/about_course.py
+++ exercises/about_course.py
@@ -17,15 +17,18 @@ def about_course():
   st.write("")
   st.image("..//pdac2021_res_est_course//images//wireframe_header.jpg", use_column_width=True)
   st.markdown("# Presenters")
-  presenter = st.selectbox("", options=['Sean Horan'])
+  presenter = st.selectbox("", options=['Sean Horan', 'Valerie Wilson', 'Pierre Landry', 'Ian Weir'])
+  split_name = presenter.split(" ")
+  lcase_name = (split_name[0] + "_" + split_name[1]).lower()
+  ucase_name = (split_name[0] + "_" + split_name[1])
   
   col1, col2 = st.beta_columns([1,2.5])
   with col1:
-    st.image("..//pdac2021_res_est_course//headshots//Sean_Horan.jpeg", use_column_width=True)
+    st.image("..//pdac2021_res_est_course//headshots//" + ucase_name + ".jpeg", use_column_width=True)
     st.markdown("**Sean Horan P.Geo - Technical Manager Geology**")
     st.markdown("<shoran@slrconsulting.com>")
   with col2:
     path = "..//pdac2021_res_est_course//text_blocks"
-    f = open(path + "//resume_sean_horan.txt", "r")
+    f = open(path + "//resume_" + lcase_name + ".txt", "r")
     st.write(f.read())
  
