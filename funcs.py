9c539ada581eec201f364c961743abea1bbccab1
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 12:42:40 -0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,2 +1,7 @@
-def get_text_block(name):
+def get_text_block(fname):
+  # this is how to read a block of text:
+  path = "..//pdac2021_res_est_course//text_blocks"
+  f = open(path + "//" + fname, "r")
+  # and then write it to the app
+  return f.read();
   
