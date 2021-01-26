1280c056a11aee585b58e49fe8db5efdb37822c7
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 12:47:11 -0500

    Update about_course.py

--- exercises/about_course.py
+++ exercises/about_course.py
@@ -23,7 +23,7 @@ def about_course():
   col1, col2 = st.beta_columns([1,2.5])
   with col1:
     st.image("..//pdac2021_res_est_course//headshots//" + ucase_name + ".jpeg", use_column_width=True)
-    text = funcs.get_text_block(title_" + lcase_name + ".txt")
+    text = funcs.get_text_block("title_" + lcase_name + ".txt")
     st.write(text)
 #     st.markdown("**Sean Horan P.Geo - Technical Manager Geology**")
 #     st.markdown("<shoran@slrconsulting.com>")
