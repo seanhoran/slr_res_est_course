dfd7b350b3562318e5204020008a3bb3e87e9122
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 12:44:05 -0500

    Update about_course.py

--- exercises/about_course.py
+++ exercises/about_course.py
@@ -1,17 +1,21 @@
 import streamlit as st
 import itertools
 import os
+import funcs
 
 def about_course():
   
   st.image("..//pdac2021_res_est_course//images//wireframe_header.jpg", use_column_width=True)
   st.title("Fundamentals of Resource Estimation")
   
-  # this is how to read a block of text:
-  path = "..//pdac2021_res_est_course//text_blocks"
-  f = open(path + "//about_course.txt", "r")
-  # and then write it to the app
-  st.write(f.read())
+#   # this is how to read a block of text:
+#   path = "..//pdac2021_res_est_course//text_blocks"
+#   f = open(path + "//about_course.txt", "r")
+#   # and then write it to the app
+#   st.write(f.read())
+  
+  text = funcs.get_text_block("about_course.txt")
+  st.write(text)
   
   st.write("")
   st.image("..//pdac2021_res_est_course//images//wireframe_header.jpg", use_column_width=True)
