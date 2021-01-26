f5e567dffa0207690bc45073a7dbbcd127e0a215
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 15:34:35 -0500

    Update variograms.py

--- exercises/variograms.py
+++ exercises/variograms.py
@@ -25,6 +25,7 @@ def variograms():
   st.title("Variogram Exercise")
   st.write("")
   st.markdown("## Model the appropriate variogram for each grade pattern observed")
+  st.markdown("Assume the figures measure 100m by 100m")
   st.markdown("### Example 1:")
   col1, col2 = st.beta_columns([1, 2])
   with col1:
