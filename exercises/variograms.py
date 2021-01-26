4fe8a66fe6ae75086fa59bab1a50b90e65b0e8fc
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 13:52:50 -0500

    Update variograms.py

--- exercises/variograms.py
+++ exercises/variograms.py
@@ -4,14 +4,21 @@ import streamlit as st
 
 def variogram(nugget=0.0, srange=100.):
   var = 1.-nugget
-  h = np.arange(150.)
+  h = np.arange(120.)
   gamma = nugget + var*((3*h)/(2*srange)-(h**3)/(2*srange**3))
   gamma[h==0]=0.
   gamma[h>srange]=1.0
   return gamma, h; 
 
 def variograms():
-  gamma, h = variogram(0.3, 60.)
-  fig, ax = plt.subplots()
-  plt.plot(h, gamma, '-r')
-  st.pyplot(fig)
+  col1, col2 = st.beta_columns([1, 2])
+  with col1:
+    nugget = st.slider('Nugget', 0.0, 1.0, 0.1, 0.05, key='nugget') 
+    srange = st.slider('Range', 0.0, 120., 100., 10., key='range') 
+  with col2:
+    gamma, h = variogram(nugget, srange)
+    fig, ax = plt.subplots()
+    plt.plot(h, gamma, '-r')
+    plt.xlabel('Range (m)')
+    plt.ylable('Gamma')
+    st.pyplot(fig)
