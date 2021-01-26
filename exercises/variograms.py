19b9ae42a9bc3c6a92ffc3af338b3446fe82e12f
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-26 15:26:22 -0500

    Update variograms.py

--- exercises/variograms.py
+++ exercises/variograms.py
@@ -15,7 +15,7 @@ def variogram(nugget=0.0, srange=100., struct_type='Spherical'):
   elif struct_type == 'Power':
     pass
   else:
-    gamma = var*(1.0-np.cos((h/srange)*np.pi))
+    gamma = nugget*(1.0-np.exp((-3*h/srange)*np.pi)*(np.cos(h/10)*np.pi))
     gamma = 1.0 - gamma
     
       
