2e1c5b0aa9914cb5291f2fa7eeca901996d768b5
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-30 06:31:50 -0500

    Update block_modelling.py

--- exercises/block_modelling.py
+++ exercises/block_modelling.py
@@ -5,6 +5,13 @@ def block_modelling():
   st.markdown("# Leapfrog View Example")
   st.markdown("Example of embedding a leapfrog view file")
   html = '<iframe src="https://view.seequent.com/embed/hbev1b2f395mzjahn1v9/default/kazbudqcim9aj33cpuiw" width=800 height=650 allowfullscreen></iframe>'
+
+  st.markdown("Embedding using markdown:")
+
   st.markdown(html, unsafe_allow_html=True)
+
+  st.markdown("Embedding using components.iframe:")
   components.iframe(html)
+
+  st.markdown("Embedding using components.html:")
   components.html(html)
