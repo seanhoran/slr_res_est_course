0a0ccc40d2187d85bc1084562dff92ee38d377bb
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-01-30 06:32:10 -0500

    Update block_modelling.py

--- exercises/block_modelling.py
+++ exercises/block_modelling.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 def block_modelling():
   st.markdown("# Leapfrog View Example")
-  st.markdown("Example of embedding a leapfrog view file")
+
   html = '<iframe src="https://view.seequent.com/embed/hbev1b2f395mzjahn1v9/default/kazbudqcim9aj33cpuiw" width=800 height=650 allowfullscreen></iframe>'
 
   st.markdown("Embedding using markdown:")
