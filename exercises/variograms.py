a0a93ad8119b55e704bbb2390c329e94a8005c9a
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-02-01 10:46:51 -0500

    Update variograms.py

--- exercises/variograms.py
+++ exercises/variograms.py
@@ -86,7 +86,7 @@ def variograms(nickname):
   
   if st.button('Submit Results'):
     host="pdac2021v1.chnzgdwh9rg1.ca-central-1.rds.amazonaws.com"
-    host = "52.60.201.96:3306"
+    host = "52.60.201.96"
     port=3306
     dbname="pdac2021_db"
     user="admin"
