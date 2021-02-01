b518de394441bb1bf99bbdc49003daee7e135f66
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-02-01 13:14:29 -0500

    Update pdac2021.py

--- pdac2021.py
+++ pdac2021.py
@@ -45,13 +45,12 @@ st.sidebar.image("..//pdac2021_res_est_course//055CF2A4-98DC-488C-B5A6-15CC02C99
 nickname = st.sidebar.text_input("*Enter Nickname")
 
 radio_options = ["01 About the Course", 
-                 "02 Database", 
-                 "03 Capping", 
-                 "04 Compositing", 
-                 "05 Variograms", 
-                 "06 Interpolation", 
-                 "07 Cut-Off Grade", 
-                 "08 Classification"]
+                 "02 Capping", 
+                 "03 Compositing", 
+                 "04 Variograms", 
+                 "05 Interpolation", 
+                 "06 Cut-Off Grade", 
+                 "07 Classification"]
 
 exercise=st.sidebar.radio("", 
                           options=radio_options, 
@@ -61,13 +60,8 @@ exercise=st.sidebar.radio("",
 if exercise == radio_options[0]:
   about_course.about_course()
 if exercise == radio_options[1]:
-  db_ex.db_ex()
-if exercise == radio_options[2]:
   capping_ex.capping_ex()
-if exercise == radio_options[4]:
-  if nickname == "":
-        st.error("Please Enter Nickname")
-  else:
-        variograms.variograms(nickname)
-if exercise == radio_options[5]:
+if exercise == radio_options[2]:
+  variograms.variograms()
+if exercise == radio_options[3]:
   block_modelling.block_modelling()
