e2e9cdbc0a97919e2801ca9661594bf771202bcc
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-02-02 12:48:30 -0500

    Update cut_off.py

--- exercises/cut_off.py
+++ exercises/cut_off.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import funcs
+import pandas as pd
 
 def cut_off():
+  
+  df = pd.read_excel("..//pdac2021_res_est_course//data//Worksheet COGs.xlsx")
+
   st.title("Cut-off Grade Exercise")
   st.write("")
   st.markdown("## Question 1: Do we send this block to the processing plant?")
@@ -29,5 +33,9 @@ def cut_off():
   st.write("")
   st.markdown("## Question 2: Complex Cut-off Calculation")
   st.write("")
-  text = funcs.get_text_block("cog_q2_intro.txt")
-  st.markdown(text)
+  col3, col4 = st.beta_columns([1, 2])
+  with col1=3:
+    text = funcs.get_text_block("cog_q2_intro.txt")
+    st.markdown(text)
+  with col1=4:
+    st.table(df)
