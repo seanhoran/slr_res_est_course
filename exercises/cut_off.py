5cd4500bf353c491750129677bd397b24c930bcd
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-02-02 12:21:26 -0500

    Update cut_off.py

--- exercises/cut_off.py
+++ exercises/cut_off.py
@@ -6,8 +6,16 @@ def cut_off():
   st.write("")
   st.markdown("## Question 1: Do we send this block to the processing plant?")
   st.write("")
-  st.markdown('### Operating costs:')
-  st.markdown('* Mining: $50/t')
-  st.markdown('* Process: $20/t')
-  st.markdown('* G&A: $15/t')
+  col1, col2 = st.beta_columns([1, 2])
+  with col1:
+    st.write("")
+    st.write("")
+    st.image("..//pdac2021_res_est_course//images//cog1_block.jpg", width=500
+  with col1:
+    st.markdown('### Operating costs:')
+    st.markdown('* Mining: $50/t')
+    st.markdown('* Process: $20/t')
+    st.markdown('* G&A: $15/t')
   
+  q1_options = ['yes', 'no']
+  cog_q1_answer = st.radio("Select the correct answer:", options=options, key='cog_q1')           
