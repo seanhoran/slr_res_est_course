cc92bf58003e4fb38e8e11f838fc5c3274d5633e
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-02-02 12:14:33 -0500

    Update cut_off.py

--- exercises/cut_off.py
+++ exercises/cut_off.py
@@ -5,3 +5,9 @@ def cut_off():
   st.title("Cut-off Grade Exercise")
   st.write("")
   st.markdown("## Question 1: Do we send this block to the processing plant?")
+  '''
+  ### Operating costs:
+  * Mining: $50/t
+  * Process: $20/t
+  * G&A: $15/t
+  '''
