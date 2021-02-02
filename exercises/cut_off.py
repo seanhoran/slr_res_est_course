2186c0ed8851c7c301cccc1545d12539e44c7c7b
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-02-02 12:15:50 -0500

    Update cut_off.py

--- exercises/cut_off.py
+++ exercises/cut_off.py
@@ -6,9 +6,8 @@ def cut_off():
   st.write("")
   st.markdown("## Question 1: Do we send this block to the processing plant?")
   st.write("")
-  st.markdown('
-  ### Operating costs:
-  * Mining: $50/t
-  * Process: $20/t
-  * G&A: $15/t
-  ')
+  st.markdown('### Operating costs:')
+  st.markdown('* Mining: $50/t')
+  st.markdown('* Process: $20/t')
+  st.markdown('* G&A: $15/t')
+  
