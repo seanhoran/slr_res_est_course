8f06253c96849f83a5130b8bee09b5ce26d306f9
phlandry <66491185+phlandry@users.noreply.github.com>
2021-02-26 12:54:16 -0800

    Update capping_ex.py

--- exercises/capping_ex.py
+++ exercises/capping_ex.py
@@ -20,7 +20,7 @@ def capping_ex():
   
   q1_answer=st.radio('Review the following histogram, probability plot and decile analysis and determine which capping level is most appropriate.', options=q1_options, index=0, key='quest1')
   
-  if q1_answer=='20 g/t Au'or if q1_answer=='25 g/t Au:
+  if q1_answer=='20 g/t Au'or '25 g/t Au':
    count_correct +=1
 
    st.image("..//pdac2021_res_est_course//images//HG_LG_Decile.jpg", use_column_width=True)
