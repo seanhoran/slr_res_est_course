3741029c47a78a4ad6eec2cc7a0cf148bde36624
phlandry <66491185+phlandry@users.noreply.github.com>
2021-02-26 11:42:00 -0800

    Update capping_ex.py

--- exercises/capping_ex.py
+++ exercises/capping_ex.py
@@ -6,7 +6,7 @@ def capping_ex():
   
   count_correct = 0
   
-  st.write("Write some stuff2")
+  st.write("The grade restriction module introduced some of the tools that can help you identify whether high grade restraining is required. This exercise uses real data and shows how the high grade restraining workflow is iterative, and that your decision to domain, cap, or otherwise restrict outlier samples should be informed by a group of tools. Lastly, it is important to note that you may elect to revisit your high grade restraining workflow after validating your estimate visually and statistically since the impact of high grade samples on the final resource estimate can be significant.")
   
   st.image("..//pdac2021_res_est_course//images//wireframe_header.jpg", use_column_width=True)
   
