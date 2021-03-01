5a841d36dece41949155a49105b4db736d410cb8
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-03-01 15:20:21 -0500

    Update reporting.py

--- exercises/reporting.py
+++ exercises/reporting.py
@@ -2,6 +2,8 @@ import streamlit as st
 import funcs
 
 def reporting():
-  pass
+  st.title("Reporting/Classification Exercise")
+  text = funcs.get_text_block("reporting.txt")
+  st.markdown(text)
   
   
