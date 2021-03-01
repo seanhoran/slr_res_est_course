baaa02efc8723c1cd899e9a9e58292798c20159b
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-03-01 15:26:47 -0500

    Update reporting.py

--- exercises/reporting.py
+++ exercises/reporting.py
@@ -5,5 +5,6 @@ def reporting():
   st.title("Reporting/Classification Exercise")
   text = funcs.get_text_block("reporting.txt")
   st.markdown(text)
+  st.image("..//pdac2021_res_est_course//images//res_table.jpg", use_column_width=True)
   
   
