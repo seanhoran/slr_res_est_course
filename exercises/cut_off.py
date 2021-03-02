f79b8d81873ad2be549e67d4f0e58fa324e77656
seanhoran <38132944+seanhoran@users.noreply.github.com>
2021-03-02 13:26:49 -0500

    Update cut_off.py

--- exercises/cut_off.py
+++ exercises/cut_off.py
@@ -74,9 +74,12 @@ def adjust_calculation(df2):
     st.table(ddf)    
   with colz2:  
     fig = px.bar(ddf, x='Metal', y='Revenue by Metal Unit', color='Metal')
+    fig.update_yaxes(range=(0,200))
     st.plotly_chart(fig,  use_container_width=True)
+    
   with colz3:
     fig = px.bar(ddf, x='Metal', y='Block Revenue per tonne', color='Metal')
+    fig.update_yaxes(range=(0,200))
     st.plotly_chart(fig,  use_container_width=True)   
            
   # st.table(df2)
