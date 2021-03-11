b4660d92de0109ce1b84e61db2d66bb4f3893089
phlandry <66491185+phlandry@users.noreply.github.com>
2021-03-11 12:02:01 -0800

    Update geo_interp.py

--- exercises/geo_interp.py
+++ exercises/geo_interp.py
@@ -31,7 +31,9 @@ def geo_interp():
 
     st.radio("How will this choice impact how I design the downstream workflow for Mineral Resource estimation?", options=q2_options, key="q2")
     st.markdown("## Answer: All of the above.")
-    st.markdown("")
+    st.write("")       
+    st.write("")
+    st.write("")
         
     col1, col2, col3 = st.beta_columns((1,1,1))
 
