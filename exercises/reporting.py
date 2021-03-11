f451e8448371ca632182dd0541e7d85db1d919f5
phlandry <66491185+phlandry@users.noreply.github.com>
2021-03-11 12:24:59 -0800

    Update reporting.py

--- exercises/reporting.py
+++ exercises/reporting.py
@@ -10,5 +10,5 @@ def reporting():
   
   st.markdown("## Answers")
   st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],}))
+    'Error Number': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+    'second column': ["MII tallied in final row.", "No information as to whether Mineral Resources and Inclusive or Exclusive.", 30, 40,50,60,70,80,90,100,110,120],}))
