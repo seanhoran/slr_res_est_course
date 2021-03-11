aa73ca4ccb2ce4f7eff8389efe0b0a7ae4cffc10
phlandry <66491185+phlandry@users.noreply.github.com>
2021-03-11 12:12:19 -0800

    Update reporting.py

--- exercises/reporting.py
+++ exercises/reporting.py
@@ -7,7 +7,7 @@ def reporting():
   st.markdown(text)
   st.image("..//pdac2021_res_est_course//images//res_table.jpg", use_column_width=True)
   
-st.markdown("## Answers"
-st.write(pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40],}))
+  st.markdown("## Answers"
+  st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],}))
