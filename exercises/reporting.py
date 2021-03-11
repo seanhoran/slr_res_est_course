f6de8707609ab78efa6ecd7c61df7baa3f448171
phlandry <66491185+phlandry@users.noreply.github.com>
2021-03-11 12:39:06 -0800

    Update reporting.py

--- exercises/reporting.py
+++ exercises/reporting.py
@@ -9,5 +9,5 @@ def reporting():
   st.image("..//pdac2021_res_est_course//images//res_table.jpg", use_column_width=True)
   
   st.markdown("## Answers")
-  st.write(pd.DataFrame({
-    'Error': ["MII tallied in final row.", "No information as to whether Mineral Resources and Inclusive or Exclusive.", "Missing information as to which cut-off grade is relevant for each ore type.", "Missing zinc metal prices.", "Missing key assumption as to whether the deposit is envisaged to be mined using underground or open pit mining.", "Missing the statement reiterating that Mineral Resources that are not Mineral Reserves do not have demonstrated economic viability.", "Too many significant figures for contained zinc.", "Too many significant figures for silver grade.", "No unit for the material quantity (Million Tonnes).", "No details on metallurgical assumptions used to generate the cut-off value.", "No information as to how the reasonable prospects for eventual economic extraction were determined.", "Missing Statement discussing issues relating to all relevant technical and economic factors likely to influence the prospect of economic extraction, and whether they can be resolved with further work."],}))
+  text = funcs.get_text_block("RPT_Answers.txt")
+  st.markdown(text)
