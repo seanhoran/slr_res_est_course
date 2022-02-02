999e091a5f6098a1a97884309dcc259101babf3f
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-02 16:26:40 -0500

    Update about_streamlit.py

--- exercises/about_streamlit.py
+++ exercises/about_streamlit.py
@@ -2,4 +2,6 @@ import streamlit as st
 
 def about_streamlit():
   st.markdown("# A Little Bit About Streamlit...")
-  st.markdown("SLR would like to thank the very kind and brilliant people at [streamlit](https://streamlit.io/) for their amazing python package and app hosting as well as the additional resources provided for the course, **FREE OF CHARGE!**.")
+  st.markdown("SLR would like to thank the very kind and brilliant people at [streamlit](https://streamlit.io/) for their amazing python package and app hosting as well as the additional resources provided for the course, **FREE OF CHARGE!**")
+  st.text("")
+  st.markdown("Streamlit has truly been a game changer for the scientific community and hopefully will catch on in the geoscience world.")
