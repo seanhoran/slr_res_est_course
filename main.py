08daf57bbe617e8d6d5700af672037bcd82e6ece
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-02 08:11:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,24 +17,24 @@ padding_bottom = 0
 COLOR = '#180c52'
 BACKGROUND_COLOR = 'white'
 
-st.markdown(
-        f"""
-<style>
-    .reportview-container .main .block-container{{
-        max-width: {max_width}px;
-        padding-top: {padding_top}rem;
-        padding-right: {padding_right}rem;
-        padding-left: {padding_left}rem;
-        padding-bottom: {padding_bottom}rem;
-    }}
-    .reportview-container .main {{
-        color: {COLOR};
-        background-color: {BACKGROUND_COLOR};
-    }}
-</style>
-""",
-        unsafe_allow_html=True,
-    )
+# st.markdown(
+#         f"""
+# <style>
+#     .reportview-container .main .block-container{{
+#         max-width: {max_width}px;
+#         padding-top: {padding_top}rem;
+#         padding-right: {padding_right}rem;
+#         padding-left: {padding_left}rem;
+#         padding-bottom: {padding_bottom}rem;
+#     }}
+#     .reportview-container .main {{
+#         color: {COLOR};
+#         background-color: {BACKGROUND_COLOR};
+#     }}
+# </style>
+# """,
+#         unsafe_allow_html=True,
+#     )
 
 
 st.sidebar.image("..//slr_res_est_course//055CF2A4-98DC-488C-B5A6-15CC02C9974E.png", width=100)
