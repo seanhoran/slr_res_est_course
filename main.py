b208985d269d8b2b21281373094682751ac1cf36
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-02 16:42:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ st.set_page_config(
      }
  )
 
-st.sidebar.image("..//slr_res_est_course//images//SLR Logo 2020_Blue.svg", width=70)
+st.sidebar.image("..//slr_res_est_course//images//SLR Logo 2020_Blue.svg")
 
 # Capping Exercise - interactive
 # Compositing - Q&A
