5e8a6d9ffc51836b0895b08919cc8871ce6eed46
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-02 16:57:06 -0500

    Update reporting.py

--- exercises/reporting.py
+++ exercises/reporting.py
@@ -3,6 +3,8 @@ import funcs
 import pandas as pd
 
 def reporting():
+  
+  st.image("..//slr_res_est_course//images//wireframe_header.jpg", use_column_width=True)
   st.title("Reporting/Classification Exercise")
   text = funcs.get_text_block("reporting.txt")
   st.markdown(text)
