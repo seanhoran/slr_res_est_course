bda717175435ee63b583cf329fca0a211d488630
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-03 08:04:05 -0500

    Update about_streamlit.py

--- exercises/about_streamlit.py
+++ exercises/about_streamlit.py
@@ -4,4 +4,6 @@ def about_streamlit():
   st.markdown("# A Little Bit About Streamlit...")
   st.markdown("SLR would like to thank the very kind and brilliant people at [streamlit](https://streamlit.io/) for their amazing python package and app hosting as well as the additional resources provided for the course, **FREE OF CHARGE!**")
   st.text("")
-  st.markdown("Streamlit has truly been a game changer for the scientific community and hopefully will catch on in the geoscience world.")
+  st.markdown("Streamlit has truly been a game changer for the scientific community and hopefully will catch on in the geoscience/mining world. Geologist are not trained")
+  
+  st.video(https://youtu.be/wL8niCeXwPI)
