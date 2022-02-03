31368d23ab524aab2d2276cb7ed10a080a223f5a
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-03 08:27:52 -0500

    Update about_streamlit.py

--- exercises/about_streamlit.py
+++ exercises/about_streamlit.py
@@ -6,12 +6,12 @@ def about_streamlit():
   st.text("")
   text = "While us geologists are not trained programmers, we often find ourselves producing custom workflows in one or another programming language. "
   text += "We often produce novel solutions to difficult problems that commercial software packages cannot cater for. "
-  text += "We provide reproducable scripts to peers and clients in difficult to use, outdated interfaces embedded in the commercial packages, "
-  text += "often coded in difficult non-readable languages. "
+  text += "We provide reproducable scripts to peers and clients in difficult to use and outdated interfaces embedded in the commercial packages, "
+  text += "often coded in difficult, syntax sensitive non-readable languages. "
   text += "Streamlit provides us with an easy to use solution that takes seconds to get yourself up and running. "
   text += "All the basic widgets you will need along with all the fun stuff that python has to offer. "
   st.markdown(text)
   st.text("")
-  st.markdown("See the video below for an example of using python and streamlit in Datamine")
+  st.markdown("See the video below for an example of using python and streamlit in Datamine. Skip to minute 10 for the [streamlit](https://streamlit.io/) demonstration.")
   
   st.video('https://youtu.be/wL8niCeXwPI')
