bbff439d7f0b73fb18c3d95138b145b068fe3d3e
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-02-03 13:24:51 -0500

    Update about_streamlit.py

--- exercises/about_streamlit.py
+++ exercises/about_streamlit.py
@@ -5,7 +5,7 @@ def about_streamlit():
   st.markdown("SLR would like to thank the very kind and brilliant people at [streamlit](https://streamlit.io/) for their amazing python package and app hosting as well as the additional resources provided for the course, **FREE OF CHARGE!**")
   st.text("")
   text = "While us geologists are not trained programmers, we often find ourselves producing custom workflows in one or another programming language. "
-  text += "We often produce novel solutions to difficult problems that commercial software packages cannot cater for. "
+  text += "We produce novel solutions to difficult problems that commercial software packages cannot cater for. "
   text += "We provide reproducable scripts to peers and clients in difficult to use and outdated interfaces embedded in the commercial packages, "
   text += "often coded in difficult, syntax sensitive non-readable languages. "
   text += "Streamlit provides us with an easy to use solution that takes seconds to get yourself up and running. "
