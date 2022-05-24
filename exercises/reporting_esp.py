565f3354f5d24b8d001abe6cee11c40b16c29463
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-05-24 16:00:01 -0400

    Update reporting_esp.py

--- exercises/reporting_esp.py
+++ exercises/reporting_esp.py
@@ -5,7 +5,7 @@ import pandas as pd
 def reporting():
   
   st.image("..//slr_res_est_course//images//wireframe_header.jpg", use_column_width=True)
-  st.title("Reporting/Classification Exercise")
+  st.title("Declaracion de Recursos Minerales")
   text = funcs.get_text_block("reporting_esp.txt")
   st.markdown(text)
   st.image("..//slr_res_est_course//images//res_table.jpg", use_column_width=True)
