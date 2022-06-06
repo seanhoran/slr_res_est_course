9ca4a5908ef0c623a12ca27d000cbe3d9ae2222c
seanhoran <38132944+seanhoran@users.noreply.github.com>
2022-06-06 09:43:52 -0400

    Update geo_interp.py

--- exercises/geo_interp.py
+++ exercises/geo_interp.py
@@ -89,5 +89,5 @@ def geo_interp():
     with colz3:
         sel_sect = st.selectbox("Display Section", options=['Si', 'Fe', 'P'], index=0, key='g1')
         st.image("..//slr_res_est_course//images//" + sel_sect + "_sect.jpg")
-    st.markdown("## Answer: Silica and iron together, phosphorus separate.")
+#     st.markdown("## Answer: Silica and iron together, phosphorus separate.")
 
